d983be90f4023731341636a8ddf3eae3129f37eb
Bill Smith <bill.smith@sharpnotions.com>
2017-10-25 17:00:22 -0400

    Donation Notification added

--- app/content/overlay.js
+++ app/content/overlay.js
@@ -128,6 +128,52 @@ function setupPage() {
     }
   };
 
+  const notification = new Vue({
+    el:'#donation_notification',
+    data:{
+      animating: false,
+      showNotification: false,
+      from: 'No one',
+      amount: 0,
+      displayName: 'Ya Boi',
+      message: '',
+      backlog: []
+    },
+    methods: {
+      show: function(data){
+        let x = null;
+        if(data){
+          if(this.backlog.length || this.animating){
+            this.backlog.push(data);
+          } else {
+            x = data;
+          }
+        } else if(this.backlog.length) {
+          x = this.backlog.shift();
+        }
+
+        if(x){
+          this.from = x.from;
+          this.amount = x.amount;
+          this.displayName = x.displayName;
+          this.message = x.message;
+          this.animating = true;
+          this.showNotification = true;
+        }
+      },
+      pop: function(){
+        console.log('POP!');
+        setTimeout(() => this.showNotification = false, 4500);
+      },
+      notifyExit: function(){
+        this.animating = false;
+        if(this.backlog.length){
+          this.show();
+        }
+      }
+    }
+  })
+
   var socket = io('/public');
   socket.on('connect', function(){
     console.log('Socket Connected');
@@ -163,7 +209,8 @@ function setupPage() {
     timers: timers,
     donationProgress: donationProgress,
     roster: roster,
-    topInfo: topInfo
+    topInfo: topInfo,
+    notification: notification
   };
 };
 
